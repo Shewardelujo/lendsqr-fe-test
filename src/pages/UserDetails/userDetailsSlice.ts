0eb0b3432f037749d9c9c75fdc8f9b76d02156ae
Shewardelujo <adesewa.adefalujo@bankly.ng>
2023-04-20 15:59:06 +0100

    Added the Each User Details Page

---
+++ src/pages/UserDetails/userDetailsSlice.ts
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface User {
+  user: {};
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null | undefined;
+}
+
+const initialState: User = { user: [], status: "idle", error: null };
+
+// Define the async thunk for fetching a user data
+export const fetchUser = createAsyncThunk(
+  "users/fetchUser",
+  async (userId: number) => {
+    try {
+      const { data } = await axios.get(
+        `https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${userId}`
+      );
+      localStorage.setItem("user", JSON.stringify(data));
+      return data;
+    } catch (error: any) {
+      throw error.response.data.message;
+    }
+  }
+);
+
+// Define the user slice
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUser.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.user = { ...state.user, ...action.payload };
+    });
+    builder.addCase(fetchUser.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default userSlice.reducer;
